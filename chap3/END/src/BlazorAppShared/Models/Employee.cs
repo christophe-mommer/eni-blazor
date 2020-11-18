cdaaeedb1d1a0ddb2800635ba3ea5be6c1b178c6
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2020-11-18 08:38:53 +0100

    chap 3 end

---
+++ chap3/END/src/BlazorAppShared/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BlazorAppShared.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string Street { get; set; }
+        public string Zipcode { get; set; }
+        public Country Country { get; set; }
+        public Job Job { get; set; }
+    }
+}
---
+++ chap3/END/src/BlazorServerApp/Pages/EmployeeDetails.razor.cs
@@ -0,0 +1,54 @@
+﻿using BlazorAppShared.Models;
+using BlazorServerApp.Services.Abstractions;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Pages
+{
+    public class EmployeeDetailsBase : ComponentBase
+    {
+        [Parameter]
+        public Guid Id { get; set; }
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        protected List<Country> Countries = new List<Country>
+        {
+            new Country{ Id = 1, Name = "France" },
+            new Country{ Id = 2, Name = "Belgique" },
+            new Country{ Id = 3, Name = "Suisse" },
+            new Country{ Id = 4, Name = "Canada" },
+            new Country{ Id = 5, Name = "Autre" }
+        };
+        protected List<Job> Jobs = new List<Job>
+        {
+            new Job { Id = 1, Title = "Manager" },
+            new Job { Id = 2, Title = "Journaliste" },
+            new Job { Id = 3, Title = "Développeur" },
+            new Job { Id = 4, Title = "Directeur" },
+            new Job { Id = 5, Title = "Secrétaire" }
+        };
+        protected Employee Employee { get; set; }
+        protected int SelectedCountryId { get; set; }
+        protected int SelectedJobId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetAll().FirstOrDefaultAsync(e => e.Id == Id);
+            SelectedCountryId = Employee.Country?.Id ?? 0;
+            SelectedJobId = Employee.Job?.Id ?? 0;
+            await base.OnInitializedAsync();
+        }
+
+        protected async Task Save()
+        {
+            Employee.Job = Jobs.First(j => j.Id == SelectedJobId);
+            Employee.Country = Countries.First(j => j.Id == SelectedCountryId);
+            await EmployeeService.AddOrUpdate(Employee);
+        }
+    }
+}
