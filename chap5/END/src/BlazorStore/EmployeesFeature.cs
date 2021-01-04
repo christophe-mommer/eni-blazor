fb7d64472b9bb9e06bccce17290b62a389086394
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2021-01-04 07:58:02 +0100

    chap 5 end

---
+++ chap5/END/src/BlazorStore/EmployeesFeature.cs
@@ -0,0 +1,18 @@
+﻿using Fluxor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStore
+{
+    public class EmployeesFeature : Feature<EmployeeState>
+    {
+        public override string GetName()
+            => "Employees";
+
+        protected override EmployeeState GetInitialState()
+            => new EmployeeState(null);
+    }
+}
---
+++ chap5/END/src/BlazorStore/EmployeesReducers.cs
@@ -0,0 +1,25 @@
+﻿using BlazorStore.Actions;
+using Fluxor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorStore
+{
+    public static class EmployeesReducers
+    {
+        [ReducerMethod]
+        public static EmployeeState ReduceEmployeeLoaded(EmployeeState state, LoadEmployees action)
+            => state with { ErrorCode = "" };
+
+        [ReducerMethod]
+        public static EmployeeState ReduceEmployeeLoaded(EmployeeState state, EmployeesLoaded action)
+            => state with { Employees = action.Employees.ToArray(), ErrorCode = "" };
+
+        [ReducerMethod]
+        public static EmployeeState ReducerEmployeeLoadingFaield(EmployeeState state, EmployeesLoadingFailed action)
+            => state with { ErrorCode = action.ErrorCode };
+    }
+}
